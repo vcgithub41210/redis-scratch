4bc2e52899019e64fcb4f3cc8c951f261be6ddbd
Vinay Chandrasekhar <vinayc.11b.kmbvm@gmail.com>
2025-08-15 23:34:51 +0530

    client program for test

--- test/client.rs
+++ test/client.rs
@@ -1,24 +1,37 @@
 #![allow(unused_imports)]
 use std::net::TcpStream;
-use std::io::{Read,Write};
+use std::io::{self, BufRead, Write, Read};
 
-fn main(){
+fn main() {
     let mut stream = TcpStream::connect("127.0.0.1:6379").unwrap();
+    let stdin = io::stdin();
 
-    let msg = String::from("PING\n");
+    println!("Connected to server at 127.0.0.1:6379");
+    println!("Type commands like: PING, ECHO hello, SET key value, GET key");
+    println!("Type EXIT to quit.");
 
-    stream.write_all(msg.as_bytes()).unwrap();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
 
-    let mut buf = [0;512];
+        let mut input = String::new();
+        stdin.lock().read_line(&mut input).unwrap();
+        let input = input.trim();
 
-    loop{
+        if input.eq_ignore_ascii_case("EXIT") {
+            break;
+        }
+
+        let msg = format!("{}\n", input);
+        stream.write_all(msg.as_bytes()).unwrap();
+
+        let mut buf = [0; 512];
         let bytes_read = stream.read(&mut buf).unwrap();
-
-        if bytes_read == 0{
+        if bytes_read == 0 {
             println!("Server closed connection");
             break;
         }
         let message = String::from_utf8_lossy(&buf[..bytes_read]);
-        println!("Received: {}",message);
+        println!("Received: {}", message);
     }
 }
