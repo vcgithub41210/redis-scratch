9868ada03b9a49b6da9d804115402cb116dc0ce1
Vinay Chandrasekhar <vinayc.11b.kmbvm@gmail.com>
2025-08-17 23:47:28 +0530

    split tokens as commands and args

--- token.rs
+++ token.rs
@@ -1,7 +1,8 @@
-pub fn parse_command(msg: &[u8], _len: usize)-> Vec<String> {
+pub fn parse_command(msg: &[u8], _len: usize) -> (String, Vec<String>) {
     let s = String::from_utf8_lossy(&msg[.._len]);
-    s.split("\r\n")
-        .filter(|line| !line.is_empty() && !line.starts_with('*') && !line.starts_with('$'))
+    let tokens = s.split("\r\n")
+        .filter(|line| !line.is_empty() && !line.startes_with("*") && !line.starts_with("$"))
         .map(|line| line.to_string())
-        .collect()
+        .collect::<Vec<String>>();
+    (tokens[0].clone(),tokens[1..].to_vec())
 }
