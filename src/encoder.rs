0c55c960f1eae555d2fd892e56370b9136623826
vcgithub41210 <vinayc.11b.kmbvm@gmail.com>
2025-08-21 20:48:32 +0530

    partial response encoder

---
+++ src/encoder.rs
@@ -0,0 +1,23 @@
+pub fn encoder(respType: u8, content: &[u8]) -> [u8] {
+    match respType {
+        ":" => {
+            //logic for resp integer
+        }
+        "+" => {
+            //logic for resp simple integer
+        }
+        "$" => {
+            //logic for resp bulk string
+        }
+        "*" => {
+            //logic for array
+        }
+    }
+    //return content
+}
+
+pub fn encoder(resType: u8, content: &[u8]) ->String {
+    match resType {
+        b'
+    }
+}
